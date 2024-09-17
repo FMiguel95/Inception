e9485c9d00db4c2d9b9f565d72967be42ba6258f
Fernando <fernando_1995_miguel@hotmail.com>
2024-09-17 05:04:37 +0100

    sql debug

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -4,12 +4,12 @@
 // These settings are pulled from environment variables so that we can access 
 // the database with the correct credentials.
 
-define( 'DB_NAME', getenv('DB_NAME') );
-define( 'DB_USER', getenv('DB_USER_NAME') );
+define( 'DB_NAME',     getenv('DB_NAME') );
+define( 'DB_USER',     getenv('DB_USER_NAME') );
 define( 'DB_PASSWORD', getenv('DB_USER_PASS') );
-define( 'DB_HOST', getenv('DB_HOST') );
-define( 'WP_HOME', getenv('WP_HOME') );
-define( 'WP_SITEURL', getenv('WP_SITEURL') );
+define( 'DB_HOST',     getenv('DB_HOST') );
+define( 'WP_HOME',     getenv('WP_HOME') );
+define( 'WP_SITEURL',  getenv('WP_SITEURL') );
 
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
@@ -27,11 +27,11 @@ $table_prefix = 'wp_';
 
 define('WP_DEBUG', true);
 
-#define( 'WP_DEBUG_LOG', true );
-#define( 'WP_DEBUG_DISPLAY', true );
-#define( 'SCRIPT_DEBUG', true );
-#error_reporting(E_ALL);
-#ini_set('display_errors', 1);
+// define( 'WP_DEBUG_LOG', true );
+// define( 'WP_DEBUG_DISPLAY', true );
+// define( 'SCRIPT_DEBUG', true );
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
 
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', __DIR__ . '/' );
