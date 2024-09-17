df6c2f68230a9a2518e971a67223a835a9b759ad
Your Name <you@example.com>
2024-09-17 18:33:10 +0100

    backup

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -4,12 +4,19 @@
 // These settings are pulled from environment variables so that we can access 
 // the database with the correct credentials.
 
-define( 'DB_NAME',     getenv('DB_NAME') );
-define( 'DB_USER',     getenv('DB_USER_NAME') );
-define( 'DB_PASSWORD', getenv('DB_USER_PASS') );
-define( 'DB_HOST',     getenv('DB_HOST') );
-define( 'WP_HOME',     getenv('WP_HOME') );
-define( 'WP_SITEURL',  getenv('WP_SITEURL') );
+// define( 'DB_NAME',     getenv('DB_NAME') );
+// define( 'DB_USER',     getenv('DB_USER_NAME') );
+// define( 'DB_PASSWORD', getenv('DB_USER_PASS') );
+// define( 'DB_HOST',     getenv('DB_HOST') );
+// define( 'WP_HOME',     getenv('WP_HOME') );
+// define( 'WP_SITEURL',  getenv('WP_SITEURL') );
+
+define( 'DB_NAME',     "wp_database" );
+define( 'DB_USER',     "dbuser" );
+define( 'DB_PASSWORD', "741" );
+define( 'DB_HOST',     "mariadb" );
+define( 'WP_HOME',     "https://fernacar.42.fr" );
+define( 'WP_SITEURL',  "https://fernacar.42.fr" );
 
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
@@ -27,11 +34,13 @@ $table_prefix = 'wp_';
 
 define('WP_DEBUG', true);
 
-// define( 'WP_DEBUG_LOG', true );
-// define( 'WP_DEBUG_DISPLAY', true );
-// define( 'SCRIPT_DEBUG', true );
-// error_reporting(E_ALL);
-// ini_set('display_errors', 1);
+define( 'WP_DEBUG_LOG', true );
+define( 'WP_DEBUG_DISPLAY', true );
+define( 'SCRIPT_DEBUG', true );
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
 
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', __DIR__ . '/' );
